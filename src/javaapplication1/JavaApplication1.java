1f75b1555d781cfd6f0fd650ff86699b8944b908
Alejandro <alejo20081701@gmail.com>
2025-09-30 14:54:32 -0500

    primer commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num=1;
+        if(num==1){
+            System.out.println("numero correcto");
+        }else if(num==2){
+            System.out.println("numero incorrecto");
+        }else{
+            System.out.println("el numero no es valido");
+        }
+    }
+    
+}
